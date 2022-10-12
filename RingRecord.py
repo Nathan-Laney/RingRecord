16daa4f26a61c95a9d485e5528e31962225215e6
Nathan Laney <nathaniel.laney@gmail.com>
2022-10-12 12:59:44 -0400

    Added datetime to filename to prevent overwriting

--- RingRecord.py
+++ RingRecord.py
@@ -2,10 +2,9 @@
 
 # import openCV library and numpy
 from collections import deque
-import cv2 
-import numpy 
-
-
+from datetime import datetime
+import cv2
+import numpy
 
 
 # define a video capture object
@@ -13,37 +12,44 @@ vid = cv2.VideoCapture(0)
 # When 0 is placed in as the parameter to VideoCapture(), this connects to the default webcam of our computer
 
 
-
 # set width and height of imported video to variables
-width= int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-height= int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # try getting FPS
 fps_float = vid.get(cv2.CAP_PROP_FPS)
 fps = int(fps_float)
-# print(fps) # print FPS to terminal 
+# print(fps) # print FPS to terminal
 
 # set a variable to store file save location
 file_save_loaction = "/home/nathan/Videos/RingRecord/"
 
 # set a variable for the fourcc video codec
-fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
 # this is the video in a buffer storing past 10 seconds.
-frames = deque(maxlen=(5 * fps)) 
+frames = deque(maxlen=(5 * fps))
 
-# Will the video save by default? 
+# Will the video save by default?
 doYouWantToSave = True
 
 def save_to_vid(video):
 
-    writer= cv2.VideoWriter( file_save_loaction + 'basicvideo.mp4', fourcc, fps, (width,height))
+    # get current date and time for filename
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y_%H:%M:%S")
+    writer = cv2.VideoWriter(file_save_loaction + 'rawVideo_' +
+                             dt_string + '.mp4', fourcc, fps, (width, height))
+    print("Saving at: " + file_save_loaction +
+          'rawVideo_' + dt_string + '.mp4')
     for frame in frames:
         writer.write(frame)
 
     writer.release()
+    print("Done!")
 
-while (True): # Once per frame
+
+while (True):  # Once per frame
 
     # Render this frame
     ret, frame = vid.read()
@@ -66,7 +72,6 @@ while (True): # Once per frame
         print("Not saving on end.")
 
 
-
 # After the loop release the cap object
 vid.release()
 # Destroy all the windows
@@ -75,9 +80,6 @@ cv2.destroyAllWindows()
 
 if doYouWantToSave:
     save_to_vid(frames)
+    
 else:
     del frames
-
-
-
-
