2475ff135c56791672ecaa16a5c22daa2657d02f
Nathan Laney <nathaniel.laney@gmail.com>
2022-10-12 12:10:57 -0400

    Add basic save video functionality

--- RingRecord.py
+++ RingRecord.py
@@ -5,12 +5,33 @@ import cv2 as cv2
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
+# When 0 is placed in as the parameter to VideoCapture(), this connects to the default webcam of our computer
 
-while (True):
+# set width and height of imported video to variables
+width= int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+height= int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    # Capture the video frame
-    # by frame
+# try getting FPS
+fps_float = vid.get(cv2.CAP_PROP_FPS)
+fps = int(fps_float)
+# print(fps) # print FPS to terminal 
+
+# set a variable to store file save location
+file_save_loaction = "/home/nathan/Videos/RingRecord/"
+
+# set a variable for the fourcc video codec
+fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+
+writer= cv2.VideoWriter( file_save_loaction + 'basicvideo.mp4', fourcc, fps, (width,height))
+
+
+while (True): # Once per frame
+
+    # Render this frame
     ret, frame = vid.read()
+
+    # using writer, save the frame 
+    writer.write(frame)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
@@ -23,5 +44,7 @@ while (True):
 
 # After the loop release the cap object
 vid.release()
+# and the writer object
+writer.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
